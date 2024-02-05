f3ce7789c67a88a6353bf4956ffe1113b5ee3bd3
ahmetfarukcuha <ahmetfarukcuha@hotmail.com>
2024-02-05 18:52:53 +0300

    initial

---
+++ server.js
@@ -0,0 +1,89 @@
+// app.js
+const express = require('express');
+const cors = require('cors')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+let Schema = mongoose.Schema
+let ObjectId = Schema.ObjectId;
+
+const noteSchema = new Schema({
+  id: ObjectId,
+  title: String,
+  createdTime: { type: Date, default: Date.now() }
+}, {
+  versionKey: false
+});
+
+const Note = mongoose.model('Note', noteSchema);
+
+const corsOpts = {
+  origin: '*',
+  credentials: true,
+  methods: ['GET','POST','HEAD','PUT','PATCH','DELETE'],
+  allowedHeaders: ['Content-Type'],
+  exposedHeaders: ['Content-Type']
+};
+app.use(cors(corsOpts))
+
+// Middleware
+app.use(bodyParser.json());
+
+app.get('/', async (req, res) => {
+  res.status(200).json({ message: "Server running!" })
+})
+
+// Get all notes
+app.get('/notes', async (req, res) => {
+  try {
+    const notes = await Note.find();
+    res.json(notes);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Insert a new note
+app.post('/notes', async (req, res) => {
+  try {
+    const { title } = req.body;
+    const newNote = new Note({ title });
+    await newNote.save();
+    res.json({ message: 'Note inserted successfully', note: newNote });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+// Delete a note
+app.delete('/notes/:id', async (req, res) => {
+  try {
+    const noteId = req.params.id;
+    await Note.findByIdAndDelete(noteId);
+    res.json({ message: 'Note deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Clear all notes
+app.delete('/notes/clear', async (req, res) => {
+  try {
+    await Note.deleteMany({});
+    res.json({ message: 'All notes cleared successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Start the server
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
